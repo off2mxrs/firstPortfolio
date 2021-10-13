9e3dc93743c9a13afc31bcb5236799534c1d856c
off2mxrs <off2mxrs@gmail.com>
2021-10-12 17:19:50 -0700

    Started Nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,18 @@ import { Link } from 'react-router-dom'
 function Header() {
    return (
        <header>
-           <h1>MARS</h1>
-           <ul>
+           <h1>
+                <Link className="header-title" to="/">MARS</Link>
+            </h1>
+           <ul className="navList">
                <li>
-                   <Link to ="/">Portfolio</Link>
+                   <Link className="navLink" to ="/">Portfolio</Link>
                </li>
                <li>
-                   <Link to ="/about">About</Link>
+                   <Link className="navLink" to ="/about">About</Link>
                </li>
                <li>
-                   <Link to ="/contact">Contact</Link>
+                   <Link className="navLink" to ="/contact">Contact</Link>
                </li>
            </ul>
        </header>
