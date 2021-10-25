a20f3843ff071e128a22ae6e9e9565b4df9a2a4f
off2mxrs <off2mxrs@gmail.com>
2021-10-25 12:42:22 -0700

    Project page layout started

--- src/components/CategoryDetail.jsx
+++ src/components/CategoryDetail.jsx
@@ -1,16 +1,48 @@
 function CategoryDetail(props) {
   return (
     <div className="category-detail">
-       <section >
-        <h2>{props.category.categoryName}</h2> 
-       </section>
+        <h2 className="page-title">Projects</h2> 
+     
 
-      <div >
-        <p>{props.category.categoryDescription}</p>
+      <div className="project" >
+        <section className="info">
+          <h3>Label</h3>
+          <br/>
+          <p>Music website to showcase artists.</p>
+          <br/>
+          <p>Created with EXPRESS and EJS</p>
+        </section>
+        <section className="gallary">
+          <img src={props.category.project[0].img} alt="" className="project-img"/>
+        </section>
       </div>
 
-      <img src={props.category.project[0].media[0]} alt="" className="category-img"/>
-      <img src={props.category.project[0].img} alt="" className="home-img"/>
+      <div className="project" >
+        <section className="info2">
+          <h3>Wayfarer</h3>
+          <br/>
+          <p>Travel website</p>
+          <br/>
+          <p>Created with REACT.</p>
+        </section>
+        <section className="gallary2">
+          <img src={props.category.project[0].img} alt="" className="project-img"/>
+        </section>
+      </div>
+
+      <div className="project" >
+        <section className="info">
+          <h3>blvd</h3>
+          <br/>
+          <p>Social media website.</p>
+          <br/>
+          <p>Created with Express and EJS.</p>
+        </section>
+        <section className="gallary">
+          <img src={props.category.project[0].img} alt="" className="project-img"/>
+        </section>
+      </div>
+
       
      
     </div>
