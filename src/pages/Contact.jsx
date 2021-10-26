74af0f11eb3bbb69095a7bd5dd2c56cba3fba8b5
off2mxrs <off2mxrs@gmail.com>
2021-10-26 00:19:33 -0700

    Contact started

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -11,9 +11,29 @@ function Contact() {
                 </h3>
             </div>
             <main className="category-info">
-                <div className="category-detail">
-                    <h2> ADD CONTACT FORM and SOCIAL MEDIA LINKS </h2>   
-                    <img src="../me.jpg" alt="" className="category-img"/>
+                <div className="category-detail"> 
+                    <div className="social-media">
+                        <img src="../me.jpg" alt="" className="contact-img"/>
+                        <p className="icons">SOCIAL MEDIA</p>
+                    </div> 
+
+                    <form>
+                        Contact
+                        <br/>
+                        <label className="#"></label>
+                        <input type="text" className="#"></input>
+                        <br/>
+                        <label className="#"></label>
+                        <input type="text" className="#"></input>
+                        <br/>
+                        <label className="#"></label>
+                        <input type="text" className="#"></input>
+                        <br/>
+                        <label className="#"></label>
+                        <input type="text" className="#"></input>
+                        <br/>
+                        <input type="submit"></input>
+                    </form> 
                 </div>
 
             </main>
