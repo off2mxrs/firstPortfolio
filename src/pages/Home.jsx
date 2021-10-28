0722165eb0027e0c2d77a06b9d0496932832131c
off2mxrs <off2mxrs@gmail.com>
2021-10-28 14:50:52 -0700

    Started animated texting

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -15,11 +15,16 @@ function Home() {
             
             <main className="category-info">
                 <div className="home-background">
-                    <br />
-                    <br />
-                    <h1>Marshawn Davidson</h1>
-                    <h2>Software Engineer | Front End Developer</h2> 
-                    {/* <img src="../9YjtagPA.jpg" alt="" className="home-img"/> */}
+                  <div className="wrapper">
+                    <div className="static-txt">Marshawn Davidson | </div>
+                    <ul className="dynamic-txt">
+                        <li><span>Software Engineer</span></li>
+                        <li><span>Front-End Specialist</span></li>
+                        <li><span>Curator</span></li>
+                        <li><span>Music Artist</span></li>
+                        <li><span>Dancer</span></li>
+                    </ul>
+                   </div>
                 </div>
             </main>
 
